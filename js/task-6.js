c874e06681a14c1bd104904a658d5ca9c31746fe
Piter30 <ditq65@gmail.com>
2024-08-12 17:53:31 +0200

    homeworks 1,2,3,4,5,6

---
+++ js/task-6.js
@@ -0,0 +1,41 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById("boxes");
+  const boxSize = 30;
+  boxesContainer.innerHTML = "";
+
+  for (let i = 0; i < amount; i++) {
+    const size = boxSize + i * 10;
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor(); // generuje losowy kolor
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById("boxes");
+  boxesContainer.innerHTML = "";
+}
+
+document.querySelector("[data-create]").addEventListener("click", () => {
+  const input = document.querySelector("input");
+  const amount = parseInt(input.value, 10); // konwertuje input.value na liczbę całkowitą w systemie dziesiętnym
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  } // walidacja zakresu wprowadzonej liczby
+
+  input.value = ""; // czyszczenie inputu
+});
+
+document.querySelector("[data-destroy]").addEventListener("click", () => {
+  destroyBoxes();
+});
